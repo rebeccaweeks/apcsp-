993cec681f82792cf8b024192dd76bef2222704f
rebecca weeks <793716@SEQ.ORG>
2018-10-18 08:48:41 -0700

    FINISHED

--- lab 1004 paddle game/sketch.js
+++ lab 1004 paddle game/sketch.js
@@ -18,20 +18,21 @@ var balls = [];
 //var chaser;
 var mouseLoc;
 var paddle ;
+var score = 0;
 
 function setup() {
-  var cnv = createCanvas(1200, 800);
+  var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(50, 0, 50);
   paddle = new Paddle (createVector (width/2,height/2), createVector (-.1,.1), 95,35, color(255,0,255)); //creates paddle
 
-   loadBalls(15);
+   loadBalls(10);
 }
 
 
 function loadBalls (numBalls){ // loads all the balls in the array and create new ball objects for the array
   for (var i = 0; i < numBalls; i++){
-    var loc = createVector(random(100,600), 20);
+    var loc = createVector(random(0,800), 20);
     var vel = createVector(random(-2,2), random(-2,2));
     var rad = 25;
     var col = color(random(0,255), random(0,255), random(0,255));
@@ -46,6 +47,7 @@ function loadBalls (numBalls){ // loads all the balls in the array and create ne
 //  The draw function is called @ 30 fps
 function draw() {
 background(20,20,20);
+text ("your score is: " + score, 50,50)
 noStroke ();
 paddle.run ();
 for (var i = 0; i < balls.length; i++){
@@ -56,20 +58,25 @@ for (var i = 0; i < balls.length; i++){
   var x = ballA.loc.x;
   //distance bt pad and ball
   var distY = abs (paddle.loc.y- y);
-  //distance of paddle ends
-//  if ((distY <2) && (x>paddle.loc.x) && (x<paddle.loc.x+250)){ // allows ball to bounce off of paddle but we dont want this
-  //  ballA.vel.y = -ballA.vel.y;
+
     var lowerEdge = paddle.loc.y + paddle.l ;//lower left paddle y coord value
     var upperEdge = paddle.loc.y //upper paddle value
     var  lEdge =paddle.loc.x ; // paddle left edge x value
     var rEdge = paddle.loc.x + paddle.w ; //right edge x value
 
-    if (x > lEdge && x < rEdge &&  y <lowerEdge &&  y > upperEdge ){
-      //fix to find if ball a is between avalue and a value and if y is between something and something.
-      balls.splice(i,1);
-      
+    if ((x > lEdge) && (x < rEdge) &&  (y < lowerEdge) &&  (y > upperEdge) && (ballA.vel.y > 0)){
+   //fix to find if ball a is between avalue and a value and if y is between something and something.
+   balls.splice(i,1);
+score = score+1;
+ } // boundaries
+ if ((x > lEdge) && (x < rEdge) &&  (y < lowerEdge) &&  (y > upperEdge) && (ballA.vel.y < 0)){ // if touches bottom balls go back to top ( calls load balls w 20 balls)
+var numBalls = balls.length+100;
+   balls = [];
+   loadBalls(numBalls);
+   for (var i = 0; i < balls.length; i++){
+     balls.run
+   }
+ }
 
-    } // boundaries
-//  }
 }
 }
