7e57fe803afd667e073d8a96b9594068d59d24c5
rebecca weeks <793716@SEQ.ORG>
2018-10-24 08:51:04 -0700

    fix

--- lab 1022/sketch.js
+++ lab 1022/sketch.js
@@ -1,23 +1,35 @@
 
 //  This is a comment
+
+var txt;
+
+function preload(){
+  // p5 has a function named "loadStrings"
+  txt = loadStrings("words.txt");
+}
 //  The setup function function is called once when your program begins
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(200, 200, 200);
   fill(200, 30, 150);
-
+  console.log(txt);
+bubbleSort()
+console.log(txt);
 }
 
 
+function bubbleSort (){
 
-
-for (var i = words.length-1; i<= 0; i--){
-  for (var j = 0; j <words.length-1; j++){
-    if (words [j] <words [i]){
-      var temp = words [j]
-      var j = words[i]
-      words [i] = temp //swap
+for (var i = txt.length-1; i<= 0; i--){
+  for (var j = 0; j <txt.length-1; j++){
+    if (txt[j] >txt[j+1]){
+      var temp = txt[j]
+       txt[j] = txt[i]
+      txt[i] = temp //swap
     }
   }
+
+}
+
 }
