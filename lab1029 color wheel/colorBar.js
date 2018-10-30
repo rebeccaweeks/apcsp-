767171d2c5ec7e11fd510c3eea51f5b916e190cf
rebecca weeks <793716@SEQ.ORG>
2018-10-30 08:46:17 -0700

    started lol

--- lab1029 color wheel/colorBar.js
+++ lab1029 color wheel/colorBar.js
@@ -4,11 +4,24 @@
 
 
 //Create a constructor function for the paddle object.   What parameters should this take?
-  function colorBar (length,width,height,col){
+  function colorBar (length,width,height,col,colAvg){
     this.length = length
     this.width= width
     this.height=height
     this.color=color;
+    this.clrAvg = clrAvg;
+
+  }
+this.run = function(){
+  this.render();
+
+}
+
+
+  this.render = function (){
+    fill (this.clr);
+  rect (this.loc.x,this.loc.y, this.width,this.height);  }
+
   }
 
 >>>>>>> ed8875f0241018754e52fa0aebd21139e62444e3
--- lab1029 color wheel/sketch.js
+++ lab1029 color wheel/sketch.js
@@ -1,167 +1,26 @@
-<<<<<<< HEAD
+var bars = [];  //array of colors
 
-//  This is a comment
-//  The setup function function is called once when your program begins
-//When the game is played, balls appear randomly and fall from the top downward. The player moves the paddle to try and eliminate a ball.
-//If the Ball hits the top of the paddle, the ball is eliminated and removed from the screen.  When a ball is eliminated, the score increases.
-// However, if a ball hits the bottom of the paddle, the round is over and a greater number of balls appear.  You need to decide how the player wins.
-//Create a constructor function for the paddle object.   What parameters should this take?
-//Create a constructor function for ball objects.  Load an array with Ball objects
-//Use acceleration to create gravity for the balls
-//n the Ball constructor function, add logic to marks a ball dead if when hits the top of the paddle.
-//In the draw function, remove (splice) all balls that are marked dead from the array
-//In the Ball constructor function, add logic to start a new round when the ball hits the bottom of the paddle
-//Each new round begins with a greater number of balls
-
-
-//var boid;
-var balls = [];
-//var chaser;
-var mouseLoc;
-var paddle ;
-var score = 0;
-
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(50, 0, 50);
-  paddle = new Paddle (createVector (width/2,height/2), createVector (-.1,.1), 95,35, color(255,0,255)); //creates paddle
-
-   loadBalls(10);
+function setup(){
+  var cnv = createCanvas(800,800) ;
+  vcnv.position ((windowWidth-width)/2,30);
+  background (20,20,20);
+  numBars = 40;
+  loadBars (numBars)
+  bubbleSort (bars);
 }
 
-
-function loadBalls (numBalls){ // loads all the balls in the array and create new ball objects for the array
-  for (var i = 0; i < numBalls; i++){
-    var loc = createVector(random(0,800), 20);
-    var vel = createVector(random(-2,2), random(-2,2));
-    var rad = 25;
-    var col = color(random(0,255), random(0,255), random(0,255));
-	var ball = new Ball (loc, vel,rad, col);
-	balls.push(ball);
+function draw (){
+  background (20,20,20,4000);
+  noStroke();
+  for (var i = 0; i<bars.length; i++){
+    bars[i].run();
   }
+}
+function loadBars(numBars){
+for (var i = 0; i<bars.length; i++){
+  var width = 800/numBars;
+  var height = 250;
+  var loc =
+  bars [i] = new createBar ()
 
 }
-
-
-
-//  The draw function is called @ 30 fps
-function draw() {
-background(20,20,20);
-text ("your score is: " + score, 50,50) 
-noStroke ();
-paddle.run ();
-for (var i = 0; i < balls.length; i++){
-  balls[i].run();
-  //balls bounce
-  var ballA = balls[i];
-  var y = ballA.loc.y;
-  var x = ballA.loc.x;
-  //distance bt pad and ball
-  var distY = abs (paddle.loc.y- y);
-
-    var lowerEdge = paddle.loc.y + paddle.l ;//lower left paddle y coord value
-    var upperEdge = paddle.loc.y //upper paddle value
-    var  lEdge =paddle.loc.x ; // paddle left edge x value
-    var rEdge = paddle.loc.x + paddle.w ; //right edge x value
-
-    if ((x > lEdge) && (x < rEdge) &&  (y < lowerEdge) &&  (y > upperEdge) && (ballA.vel.y > 0)){
-   //fix to find if ball a is between avalue and a value and if y is between something and something.
-   balls.splice(i,1);
-score = score+1;
- } // boundaries
- if ((x > lEdge) && (x < rEdge) &&  (y < lowerEdge) &&  (y > upperEdge) && (ballA.vel.y < 0)){ // if touches bottom balls go back to top ( calls load balls w 20 balls)
-var numBalls = balls.length+100;
-   balls = [];
-   loadBalls(numBalls);
-   for (var i = 0; i < balls.length; i++){
-     balls.run
-   }
- }
-
-}
-}
-=======
-
-//  This is a comment
-//  The setup function function is called once when your program begins
-//When the game is played, balls appear randomly and fall from the top downward. The player moves the paddle to try and eliminate a ball.
-//If the Ball hits the top of the paddle, the ball is eliminated and removed from the screen.  When a ball is eliminated, the score increases.
-// However, if a ball hits the bottom of the paddle, the round is over and a greater number of balls appear.  You need to decide how the player wins.
-//Create a constructor function for the paddle object.   What parameters should this take?
-//Create a constructor function for ball objects.  Load an array with Ball objects
-//Use acceleration to create gravity for the balls
-//n the Ball constructor function, add logic to marks a ball dead if when hits the top of the paddle.
-//In the draw function, remove (splice) all balls that are marked dead from the array
-//In the Ball constructor function, add logic to start a new round when the ball hits the bottom of the paddle
-//Each new round begins with a greater number of balls
-
-
-//var boid;
-var balls = [];
-//var chaser;
-var mouseLoc;
-var paddle ;
-var score = 0;
-
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(50, 0, 50);
-  paddle = new Paddle (createVector (width/2,height/2), createVector (-.1,.1), 95,35, color(255,0,255)); //creates paddle
-
-   loadBalls(10);
-}
-
-
-function loadBalls (numBalls){ // loads all the balls in the array and create new ball objects for the array
-  for (var i = 0; i < numBalls; i++){
-    var loc = createVector(random(0,800), 20);
-    var vel = createVector(random(-2,2), random(-2,2));
-    var rad = 25;
-    var col = color(random(0,255), random(0,255), random(0,255));
-	var ball = new Ball (loc, vel,rad, col);
-	balls.push(ball);
-  }
-
-}
-
-
-
-//  The draw function is called @ 30 fps
-function draw() {
-background(20,20,20);
-text ("your score is: " + score, 50,50) 
-noStroke ();
-paddle.run ();
-for (var i = 0; i < balls.length; i++){
-  balls[i].run();
-  //balls bounce
-  var ballA = balls[i];
-  var y = ballA.loc.y;
-  var x = ballA.loc.x;
-  //distance bt pad and ball
-  var distY = abs (paddle.loc.y- y);
-
-    var lowerEdge = paddle.loc.y + paddle.l ;//lower left paddle y coord value
-    var upperEdge = paddle.loc.y //upper paddle value
-    var  lEdge =paddle.loc.x ; // paddle left edge x value
-    var rEdge = paddle.loc.x + paddle.w ; //right edge x value
-
-    if ((x > lEdge) && (x < rEdge) &&  (y < lowerEdge) &&  (y > upperEdge) && (ballA.vel.y > 0)){
-   //fix to find if ball a is between avalue and a value and if y is between something and something.
-   balls.splice(i,1);
-score = score+1;
- } // boundaries
- if ((x > lEdge) && (x < rEdge) &&  (y < lowerEdge) &&  (y > upperEdge) && (ballA.vel.y < 0)){ // if touches bottom balls go back to top ( calls load balls w 20 balls)
-var numBalls = balls.length+100;
-   balls = [];
-   loadBalls(numBalls);
-   for (var i = 0; i < balls.length; i++){
-     balls.run
-   }
- }
-
-}
-}
->>>>>>> ed8875f0241018754e52fa0aebd21139e62444e3
