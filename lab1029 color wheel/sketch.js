81e4085dd235f0dd959fb67092d0ed2a781b8011
rebeccaweeks <43447472+rebeccaweeks@users.noreply.github.com>
2018-10-30 22:37:59 -0700

    updated, added bub sort

--- lab1029 color wheel/sketch.js
+++ lab1029 color wheel/sketch.js
@@ -20,7 +20,24 @@ function loadBars(numBars){
 for (var i = 0; i<bars.length; i++){
   var width = 800/numBars;
   var height = 250;
-  var loc =
+  var loc = createVector (o +(width*i),150);
+  var red = random (0,255);
+  var blue = random (0,255);
+  var green = random (0,255);
+  var clrAvg = (red+blue+green) / 3;
+  var bar = new colorBar (loc, width, height, color, clrAvg); //creates new bar obj
+  bars.push(bar)
   bars [i] = new createBar ()
 
 }
+function bubbleSort(Bars){
+  var length = Bars.length;
+  for (var i = 0; i<bars.length; i++){
+    for (var j = 0, j<bars.length-i-1);j++){
+      var clrA = bars[j].clrAvg;
+      var clrB = bars[j+1].clrAvg;
+      if (clrA > clrB){
+        var temp = bars[j];
+        bars [j] = bars[j+1];
+        bars [j+1] = temp;
+      }
