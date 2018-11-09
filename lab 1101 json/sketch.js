0decc549ac4a52cd4bfe34688fc32f1113f58c9b
rebecca weeks <793716@SEQ.ORG>
2018-11-09 08:47:52 -0800

    edits by mr e

--- lab 1101 json/sketch.js
+++ lab 1101 json/sketch.js
@@ -1,38 +1,50 @@
 
-var data = [];
+var data;
 var barHeight;
 //  preload a large file
 //function preload(){
 //  data = loadJSON("population.json");
 //}
 function setup(){
-  console.log("setting up");
-  loadJSON("population.json",getData);
-  bubbleSort (data);
-}
-function getData(jData){
-  console.log("got data");
-  data = jData;
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 50);
+  loadJSON("population2.json",getData);
+  if(data){
+    bubbleSort();
+  }
 
+}
+
+function getData(jData){
+  data = jData;
+  console.log(data);
 }
 
 
 function draw (){
-if (data){
-  data.population[3].females
-    console.log (data);
-}
+  background(20);
+  stroke(255);
+  fill(255);
+
+  if (data) {
+    console.log("Wazzup");
+    data.population[3].females
+    text(data.countrydata[2].males, 50, 50);
+    text("Hello Becca", 50, 50);
+  }
 }
 
 
-    function bubbleSort (){
-      for (var i = 0; i<data.length; i++){
-        for (var j = 0; j <data.length-i-1; j++){
-          if (data[j] >data[j+1]){
-              var temp = data[j];
-               data[j] =data[j+1];
-                 data[j+1] = temp; //swap
-               }
-             }
-           }
-         }
+function bubbleSort (){
+  console.log("sorting");
+  var d = data.countrydata
+  for (var i = d.length-1; i>0; i--){
+    for (var j = 0; j < i; j++){
+      if (d[j].females >d[j+1].females){
+        var temp = d[j];
+        d[j] =d[j+1];
+        d[j+1] = temp; //swap
+      }
+    }
+  }
+}
