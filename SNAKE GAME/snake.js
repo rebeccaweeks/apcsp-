2fa91ab3716046822c24aa6e9131ae6227580d1e
rebecca weeks <793716@SEQ.ORG>
2018-11-19 08:48:36 -0800

    psuedo code

--- SNAKE GAME/snake.js
+++ SNAKE GAME/snake.js
@@ -3,7 +3,7 @@
 //  The setup function function is called once when your program begins
 function Snake (loc,vel){
   this.loc = loc;
-  this.vel= vel; //movement
+  this.vel= vel; //movement of snake
   this.segments = [];
   //var createVector
 
@@ -14,10 +14,11 @@ function Snake (loc,vel){
 
   }
 
-this.update = function (){
+this.update = function (){ //location of head 
   this.loc.add (this.vel);
   this.loc.x = constrain (this.loc.x,0,width-w);
   this.loc.y = constrain (this.loc.y,0,height-w);
+  //update location
 // add new segment
 //move segment to follow the one in front
 }
@@ -25,10 +26,14 @@ this.update = function (){
 this.render = function (){
   fill (0,235,0);
   rect (this.loc.x, this.loc.y, w,w)
-
+//
 }
+//  create new segment at end of snake object
+
+
 //this.newSegment = function(){
-  //  var createVector =
+  //  var createVector
+
     //
     //newSegment.push (segments)
 }
