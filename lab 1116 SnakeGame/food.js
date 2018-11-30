555548a508221807467636c27b1e1f5989291048
rebeccaweeks <43447472+rebeccaweeks@users.noreply.github.com>
2018-11-29 22:46:18 -0800

    finish

--- lab 1116 SnakeGame/food.js
+++ lab 1116 SnakeGame/food.js
@@ -2,51 +2,37 @@
 // becca weeks
 
 
- function Food(location,){
-   this.loc = location;
+ Food.js
 
+function Food() {
+  this.vec = randomVector().mult(scl);
 
-
-  // This function calls other functions
-  this.run = function(){
-    //this.checkEdges();
-  //  this.update();
-    this.render();
+  this.x = function() {
+    return this.vec.x;
   }
-  // changes the location of the ball
-  //  adds speed to x and y
-   this.update = function(){
-      this.vel.add(this.acc);
-      this.loc.add(this.vel);
-   }
 
-   //checkEdges() reverses speed when  ball touches an edge
-   this.checkEdges = function(){
-
-   }
-
-   // render() draws the ball at  new location
-  this.render = function(){
-    fill(color(255,0,0));
-    rect(this.loc.x, this.loc.y,w,w);
+  this.y = function() {
+    return this.vec.y;
   }
-  var createFood = function() {
-          food = {
-            //Generate random numbers.
-            x: Math.floor((Math.random() * 30) + 1),
-            y: Math.floor((Math.random() * 30) + 1)
-        }
+
+  this.draw = function() {
+    fill(255, 255, 255);
+    rect(this.x(), this.y(), scl, scl);
+  }
+
+  this.eaten = function() {
+    this.vec = randomVector().mult(scl);
+  }
+}
+
+
 
         //Look at the position of the snake's body.
-        for (var i=0; i>segment.length; i++) {
-            var snakeX = Snake.segment[i].x;
-            var snakeY = Snake.segment[i].y;
-
-             if (food.x===snakeX || food.y === snakeY || food.y === snakeY && food.x===snakeX) {
-                food.x = Math.floor((Math.random() * 20) + 1);
-                food.y = Math.floor((Math.random() * 20) + 1); //move the food one cell up
-            }
-        }
-    }
-
-}
+      //  for (var i=0; i>segment.length; i++) {
+         //   var snakeX = Snake.segment[i].x;
+          //  var snakeY = Snake.segment[i].y;
+//
+         //    if (food.x===snakeX || food.y === snakeY || food.y === snakeY && food.x===snakeX) {
+              //  food.x = Math.floor((Math.random() * 20) + 1);
+               // food.y = Math.floor((Math.random() * 20) + 1); //move the food one cell up
+     
